8d696592604ad4cf15aa285598ca0d0de1f94370
Afifvc <afif512@protonmail.ch>
2021-11-02 16:47:57 +0100

    -création du repo pour entraînement aux api en vue de l'ecf -création du modèle pour l'api

--- src/main/java/apiTraining/Dinodragon/SomeDude/SomeDudeRepository.java
+++ src/main/java/apiTraining/Dinodragon/SomeDude/SomeDudeRepository.java
@@ -3,6 +3,15 @@ package apiTraining.Dinodragon.SomeDude;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
 
+import java.util.*;
+
 @Repository
 public interface SomeDudeRepository extends JpaRepository<SomeDude, Integer> {
+
+    //Récupère la liste de toutes les personnes fan de dino
+    List<SomeDude> findByDinofanIsTrue();
+
+    List<SomeDude> findByAgeIsGreaterThan(Integer age);
+
+
 }
--- src/main/java/apiTraining/Dinodragon/SomeDude/SomeDudeService.java
+++ src/main/java/apiTraining/Dinodragon/SomeDude/SomeDudeService.java
@@ -4,4 +4,11 @@ import org.springframework.stereotype.*;
 
 @Service
 public class SomeDudeService {
+private final SomeDudeRepository someDudeRepository;
+
+    public SomeDudeService(SomeDudeRepository someDudeRepository) {
+        this.someDudeRepository = someDudeRepository;
+    }
+
+    public List<>
 }
